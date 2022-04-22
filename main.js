c7ebccb8217ac82adfefea502b0ecd91f0facd04
Adriel <adrielsilva_1990@hotmail.com>
2022-04-21 23:12:10 -0300

    implementando novas funcionalidades no site

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
+// abre e fecha com o menu quando clicar no ícone superior direita
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
+
+// quando clicar em um item do menu, esconder o menu//
+const links = document.querySelectorAll('nav ul a')
+
+for (const link of links) {
+    link.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+}
