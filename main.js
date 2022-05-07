13e8da21ea4a2be1a1dfb5347f919051f3959ed9
Adriel <adrielsilva_1990@hotmail.com>
2022-05-07 20:47:18 -0300

    Consertando alguns erros de HTML e CSS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// abre e fecha com o menu quando clicar no ícone superior direita
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -8,8 +8,8 @@ for (const element of toggle) {
     })
 }
 
-// quando clicar em um item do menu, esconder o menu//
-const links = document.querySelectorAll('nav ul a')
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
     link.addEventListener('click', function () {
@@ -18,64 +18,60 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-function changeHeaderWhenScroll(){
+function changeHeaderWhenScroll() {
     const header = document.querySelector('#header')
     const navHeight = header.offsetHeight
 
-    if (this.window.scrollY >= navHeight) {
-        //scroll é maior que a  altura do header
+    if (window.scrollY >= navHeight) {
+        // scroll é maior que a altura do header
         header.classList.add('scroll')
     } else {
         // menor que a altura do header
         header.classList.remove('scroll')
-
     }
 }
 
-
-
-/* Testimonials carousel slider swiper(carrosel de rolar os depoimentos)*/
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     pagination: {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true,
+    keyboard: true
 })
 
-/* ScrollReveal: Mostrar elementos quando der scroll na pag*/
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
     origin: 'top',
     distance: '30px',
     duration: 700,
-    reset:true
+    reset: true
 })
 
 scrollReveal.reveal(
     `#home .image, #home .text,
-    #about .image, #about .text,
-    #services header, #services .card,
-    #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
-    footer .brand, footer .social
-`,
-{interval: 100}
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+    { interval: 100 }
 )
 
+/* Botão voltar para o topo */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
 
-/* Botao voltar para o topo */
-function backToTop(){
-const backToTopButton = document.querySelector('.back-to-top')
-
-    if(window.scrollY >= 560){
+    if (window.scrollY >= 560) {
         backToTopButton.classList.add('show')
     } else {
         backToTopButton.classList.remove('show')
     }
 }
 
-/*When Scroll */
+/* When Scroll */
 window.addEventListener('scroll', function () {
     changeHeaderWhenScroll()
     backToTop()
